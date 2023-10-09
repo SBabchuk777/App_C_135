0dedac40f376271a6808421d26ff08cba9146514
00Baza00 <m.ba@dcjudgement.com>
2023-10-09 03:53:45 +0200

    Added logic to Shop scene

--- Assets/Scripts/Controllers/SceneControllers/ShopController.cs
+++ Assets/Scripts/Controllers/SceneControllers/ShopController.cs
@@ -1,10 +1,43 @@
+using System.Collections.Generic;
+using Models.Game;
+using Tools.UnityAdsService.Scripts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 namespace Controllers.SceneControllers
 {
     public class ShopController : AbstractController
     {
+        [Space(5)] [Header("UI")] 
+        [SerializeField]
+        private Text _coinCountText;
+        [SerializeField] 
+        private Button _backBtn;
+        [SerializeField] 
+        private Button _buyFirstBoostBtn;
+        [SerializeField] 
+        private Button _buySecondBoostBtn;
+        [Space(5)] [Header("AdButtons")] 
+        [SerializeField]
+        private List<UnityAdsButton> _adButtons;
+
+        private GameModel _model;
+        
         protected override void OnEnableScene()
         {
+            _model = new GameModel();
             
+            SetCoinsText();
+            CheckActiveBuyBtns();
+            
+            _backBtn.onClick.AddListener(LoadSceneMenu);
+            _buyFirstBoostBtn.onClick.AddListener(PressBuyFirstBoostBtn);
+            _buySecondBoostBtn.onClick.AddListener(PressBuySecondBoostBtn);
+
+            _adButtons[0].OnCanGetReward += GetReward;
+            _adButtons[1].OnCanGetReward += GetReward;
+            _adButtons[2].OnCanGetReward += GetReward;
         }
 
         protected override void OnStartScene()
@@ -14,7 +47,53 @@ namespace Controllers.SceneControllers
 
         protected override void OnDisableScene()
         {
+            _backBtn.onClick.RemoveAllListeners();
+            _buyFirstBoostBtn.onClick.RemoveAllListeners();
+            _buySecondBoostBtn.onClick.RemoveAllListeners();
             
+            _adButtons[0].OnCanGetReward -= GetReward;
+            _adButtons[1].OnCanGetReward -= GetReward;
+            _adButtons[2].OnCanGetReward -= GetReward;
+        }
+
+        private void SetCoinsText()
+        {
+            _coinCountText.text = CoinCount.ToString();
+        }
+
+        private void PressBuyFirstBoostBtn()
+        {
+            _model.SameCellsBoosterCount++;
+            CoinCount -= 100;
+            CheckActiveBuyBtns();
+            SetCoinsText();
+        }
+        
+        private void PressBuySecondBoostBtn()
+        {
+            _model.OneColumnBoosterCount++;
+            CoinCount -= 100;
+            CheckActiveBuyBtns();
+            SetCoinsText();
+        }
+
+        private void CheckActiveBuyBtns()
+        {
+            _buyFirstBoostBtn.interactable = CoinCount >= 100;
+            _buySecondBoostBtn.interactable = CoinCount >= 100;
+        }
+
+        private void GetReward(int value)
+        {
+            CoinCount += value * 500;
+            
+            SetCoinsText();
+            CheckActiveBuyBtns();
+        }
+
+        private void LoadSceneMenu()
+        {
+            SceneManager.LoadScene("Menu");
         }
     }
 }
